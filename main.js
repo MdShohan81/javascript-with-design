1896be6ae5a7fccea4e66fa60a7c454f4e482c97
MdShohan81 <mdshohan6563@gmail.com>
2022-02-08 20:36:30 +0600

    added new comment box

---
+++ main.js
@@ -0,0 +1,14 @@
+// common submit button
+// let submitButton = document.getElementById('submit-btn');
+
+// submitButton.onclick = makeBlue;
+// const textColor = 'white';
+// function makeBlue(){
+//     document.body.style.backgroundColor = 'black';
+//     document.body.style.color = textColor;
+// }
+// function blue(){
+//     document.body.style.backgroundColor = 'blue';
+    
+//     document.body.style.color = textColor;
+// }
